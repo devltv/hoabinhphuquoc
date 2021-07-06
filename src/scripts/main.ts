ff8f3813682964aca899556bd6d1dbdb0e1be2a1
Lương Vinh <luongthevinh.dev@gmail.com>
2021-07-06 16:41:56 +0700

    start project

---
+++ src/scripts/main.ts
@@ -0,0 +1,25 @@
+declare var Swiper : any;
+
+// Common function 
+
+// Layout
+import {header} from "./layout/header";
+
+
+// Page
+
+// function Layout
+function layout(){
+  header();
+}
+
+
+// function Page
+function page(){
+  
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  layout();
+  page();
+});
