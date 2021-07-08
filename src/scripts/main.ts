a29fc7ef6c773c10bcd28d7bb53bf5b539d526e1
Lương Vinh <luongthevinh.dev@gmail.com>
2021-07-08 11:54:57 +0700

    bannerSub

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,25 +1,44 @@
+// https://fancyapps.com/docs/ui/fancybox
+// https://www.npmjs.com/package/axios
+// https://greensock.com/
+// https://swiperjs.com/get-started
 declare var Swiper : any;
 
-// Common function 
+import axios from "axios";
+
+
+//////////////////////////////////////////////////////////
+// Common function
+import * as fetchData from "./components/fetchData";
 
 // Layout
 import {header} from "./layout/header";
 
-
 // Page
+
+
+///////////////////////////////////////////////////////////////
+// function Common
+function common(){
+
+  fetchData;
+}
 
 // function Layout
 function layout(){
   header();
 }
 
-
 // function Page
 function page(){
   
 }
 
+////////////////////////////////////////////////////////////////
 document.addEventListener("DOMContentLoaded", function(){
+  common();
   layout();
   page();
 });
+
+
