f2bf06da0969a5e712ea6c78f8903919735182cb
Lương Vinh <luongthevinh.dev@gmail.com>
2021-07-09 14:49:44 +0700

    fix index-2

--- src/scripts/page/index.ts
+++ src/scripts/page/index.ts
@@ -3,11 +3,11 @@ declare var Swiper:any;
 function index2(){
   if (document.getElementById('index2')){
     new Swiper(".index-restroom--swiper", {
-        slidesPerView: 3,
-        spaceBetween: 30,
+        slidesPerView: 2,
+        spaceBetween: 8,
         // slidesPerGroup: 3,
-        loop: true,
-        loopFillGroupWithBlank: true,
+        // loop: true,
+        // loopFillGroupWithBlank: true,
         // pagination: {
         //   el: ".swiper-pagination",
         //   clickable: true,
@@ -17,6 +17,12 @@ function index2(){
           prevEl: ".swiper-button-prev",
         },
         breakpoints:{
+          1281:{
+            slidesPerView: 3,
+          },
+          1681:{
+            spaceBetween: 18
+          }
         }
       });
   }
